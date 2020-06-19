cc708653b956c029e98d4462492c15c15e79371a
Ivan Blazevic <ivan.blazevic@gmail.com>
2020-06-19 17:36:16 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+# To install this script on the server following steps are required:
+# 
+# sudo cp build.sh /usr/local/bin/build
+# sudo chmod +x /usr/local/bin/build
+
+echo "Build has started..."
