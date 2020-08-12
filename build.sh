f99e96912a5890aa85dee728779b231fc6990787
Blazh <iblazevi@gmail.com>
2020-08-12 15:45:49 +0200

    Remove service before update

--- build.sh
+++ build.sh
@@ -4,13 +4,14 @@
 # wget https://raw.githubusercontent.com/ivanblazevic/pipeline/master/build.sh
 # sudo cp build.sh /usr/local/bin/build
 # sudo chmod +x /usr/local/bin/build
+VERSION=1.0.0
 
 if [ "$1" = "update" ]
 then
     wget -P /tmp https://raw.githubusercontent.com/ivanblazevic/pipeline/master/build.sh
     sudo cp /tmp/build.sh /usr/local/bin/build
     rm /tmp/build.sh
-    echo "Build script has been updated..."
+    echo "Build script has been updated to version ${VERSION}"
     exit
 fi
 
@@ -37,12 +38,14 @@ do
   docker push localhost:5000/$DOCKER_IMAGE
 
   if [ "release" = "$branch" ]; then
+    docker service rm $SERVICE_NAME
     git --work-tree=./path/under/root/dir/live-site/ checkout -f $branch
     docker service update $SERVICE_NAME --image localhost:5000/$DOCKER_IMAGE
     echo 'Changes pushed live.'
   fi
 
   if [ "master" = "$branch" ]; then
+    docker service rm $SERVICE_NAME_DEV
     git --work-tree=./path/under/root/dir/dev-site/ checkout -f $branch
     docker service update $SERVICE_NAME_DEV --image localhost:5000/$DOCKER_IMAGE
     echo 'Changes pushed to dev.'
